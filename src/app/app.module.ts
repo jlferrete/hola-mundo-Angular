fdb653d7286b90598487fa41a49baa0203e911c2
JlFerrete <joseluisferrete@gmail.com>
2019-09-23 19:54:10 +0200

    Added footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { BodyComponent } from "./components/body/body.component";
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, BodyComponent],
+  declarations: [AppComponent, HeaderComponent, BodyComponent, FooterComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,7 +1,10 @@
+// tslint:disable-next-line: quotemark
 import { Component } from "@angular/core";
 
 @Component({
+  // tslint:disable-next-line: quotemark
   selector: "app-body",
+  // tslint:disable-next-line: quotemark
   templateUrl: "./body.component.html"
 })
 export class BodyComponent {}
